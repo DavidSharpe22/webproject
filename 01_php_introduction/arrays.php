8705f29a50e7800443f82cc61d42adce29baaa7f
David Sharpe <davidasharpe@me.com>
2023-09-12 21:15:07 +1000

    Update

---
+++ 01_php_introduction/arrays.php
@@ -0,0 +1,52 @@
+<?php 
+
+// Creating an array
+
+$daysOfTheWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
+
+$daysOfTheWeek = array("Monday", "Tuesday", "Wednesday", "Thursday", "Friday");
+
+$course = ["title" => "Web Development", "code" => "ICA50611", "version" => 2];
+
+$course = array("title" => "Web Development", "code" => "ICA50611", "version" => 2);
+
+echo $course["title"];
+
+
+// Changing and adding elements
+
+
+
+
+?>
+
+<?php
+
+// Activity 1 Creating arrays
+
+$daysOfTheWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
+
+  for ($i=0; $i<5; $i++)
+{
+echo $daysOfTheWeek[$i] . "\n"; }
+
+$course = ["title" => "Web Development", "code" => "ICA50611", "version" => 2]; echo $course["title"];
+
+echo $course["title"];
+
+
+?>
+
+
+<?php
+
+
+
+?>
+
+
+
+
+
+
+?>
